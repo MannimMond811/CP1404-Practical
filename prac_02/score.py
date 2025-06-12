7961daf4b3311370f073040daf26466ba0580177
zyyyyy <yue.zhou@my.jcu.edu.au>
2025-06-12 16:21:34 +0800

    Add starter files

---
+++ prac_02/score.py
@@ -0,0 +1,28 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+import random
+
+
+def main():
+    """main function"""
+    score = random.randint(0, 100)
+    result = determine_score(score)
+    print(f"Your result is {result}")
+
+
+def determine_score(score):
+    """determine what level the inputted score is"""
+    if score < 0 or score > 100:
+        result = "Invalid score"
+    elif score > 90:
+        result = "Excellent"
+    elif score > 50:
+        result = "Passable"
+    else:
+        result = "Bad"
+    return result
+
+
+main()
---
+++ prac_02/temprature.py
@@ -0,0 +1,38 @@
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+
+
+def main():
+    """main function"""
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = calculate_fahrenheit(celsius)
+            print(f"Result: {fahrenheit:.2f} F")
+        elif choice == "F":
+            fahrenheit = float(input("fahrenheit: "))
+            celsius = calculate_celsius(fahrenheit)
+            print(f"Result: {celsius:.2f} C")
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def calculate_celsius(fahrenheit):
+    """calculate from fahrenheit to celsius"""
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def calculate_fahrenheit(celsius):
+    """calculate from celsius to fahrenheit"""
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+main()
+
